02132d34682a4c95968193f3d1696f6a6cdeef30
Dargor94 <franciscomasera1994@gmail.com>
2023-07-26 23:22:42 -0300

    base package renamed

--- src/main/java/org/dargor/discovery/PromPathDiscoveryServerApplication.java
+++ src/main/java/org/dargor/discovery/PromPathDiscoveryServerApplication.java
@@ -1,4 +1,4 @@
-package com.example.discovery;
+package org.dargor.discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/org/dargor/discovery/PromPathDiscoveryServerApplicationTests.java
+++ src/test/java/org/dargor/discovery/PromPathDiscoveryServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.discovery;
+package org.dargor.discovery;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
